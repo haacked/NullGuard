f563b993596565ebabe6c1a5cccf6dcc986a9be1
Fabio Salvalai <fabio.salvalai@frontiersin.org>
2014-12-22 16:45:54 +0100

    Update assembly / file version

--- NullGuard.PostSharp/Properties/AssemblyInfo.cs
+++ NullGuard.PostSharp/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
 [assembly: AssemblyInformationalVersion("0.1.0")]
