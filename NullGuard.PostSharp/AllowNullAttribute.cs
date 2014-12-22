1e1c3ddbfdb3fc71a9cacf783c5e157b0a57a352
Fabio Salvalai <fabio.salvalai@frontiersin.org>
2014-12-22 16:37:54 +0100

    Recover AllowNullAttribute  for backwards compatibility, but mark it as deprecated.

---
+++ NullGuard.PostSharp/AllowNullAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NullGuard.PostSharp
+{
+    [Obsolete("Use CanBeNullAttribute instead in order to improve ReSharper's static analysis.")]
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.Property)]
+    public class AllowNullAttribute : Attribute
+    {
+    }
+}
